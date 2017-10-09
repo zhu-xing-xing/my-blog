d0858e57ea6cbcac8f7261f89ad73e4a29d73314
zhu-xing-xing <zhuxingxing3721@163.com>
2017-10-09 16:51:30 +0800

    '3,渲染模版'

--- routes/article.js
+++ routes/article.js
@@ -2,6 +2,6 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/add',function(req,res){
-	res.send('添加文章')
+	res.render('article/add',{title:'发表文章'});//不能写/或者./  不用写后缀
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 let express = require('express');
+let path = require('path');  //3,为了处理路径,引入path模块
+
 let app = express();
+
+app.set('view engine','html');  //1,设置模版引擎  指定模版的后缀为html
+app.set('views',path.resolve('views')); //2,设置模版存放的根目录,需要制定绝对路径
+//4,path.resolve从当前绝对路径出发,然后用当前的绝对路径加上views,变成另外一个绝对路径,会指向views文件夹
+//5,自定义渲染方法
+app.engine('html',require('ejs').__express); //6,指定对于html类型的模版使用ejs方法来进行渲染
+
+//此静态文件中间件会拦截客户端对于静态文件的请求,如:bootstrap.css,然后会在当前目录下的node_modules目录寻找此文件,
+// 如果能找到则返回客户端并结束请求
+app.use(express.static(path.resolve('node_modules')));//7,引入中间件,指定目录
 
 let index = require('./routes/index');//导入路由中间件
 app.use('/',index);//使用中间件 当客户端访问/时,交给index来处理
