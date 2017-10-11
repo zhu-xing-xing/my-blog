8da75dc4fd7c64a0627ddfc30c41f5a7b506572d
zhu-xing-xing <zhuxingxing3721@163.com>
2017-10-11 14:52:39 +0800

    '10,实现发表文章功能'

--- model/index.js
+++ model/index.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose');//1,引入mongoose
+let ObjectId = mongoose.Schema.Types.ObjectId;//10.1
 mongoose.connect('mongodb://127.0.0.1/my-blog',{ //2,连接数据库
 	useMongoClient: true
 });
@@ -10,9 +11,18 @@ let UserSchema = new mongoose.Schema({
 	email:String,
 	avatar:String
 });
-
 //4,定义用户模型
 let User = mongoose.model('User',UserSchema);
-
 //5,导出模型  把用户模型挂载到导出对象上
 exports.User = User;
+
+//10.2
+let ArticleSchema = new mongoose.Schema({
+	title:String,  //标题
+	content:String,  //正文/内容
+	creatAt:{type:Date,default:Date.now},   //文章创建的时间,取当前时间
+	//user是一个外键(引用的是另外一个集合(User)的主键)
+	user:{type:ObjectId,ref:'User'}  //用户(文章的作者),引用的是用户表的主键_id
+});
+let Article = mongoose.model('Article',ArticleSchema);
+exports.Article = Article;
--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,29 @@
 let express = require('express');
 let {checkNotLogin,checkLogin} = require('../auth');  //7.9  引入新写的两个中间件
 let router = express.Router();
+let {Article} =require('../model');//10.4 导入模型,并解构出来
 
 router.get('/add',checkLogin,function(req,res){//7.10
 	res.render('article/add',{title:'发表文章'});//不能写/或者./  不用写后缀
 });
+
+//10.1 写路由
+router.post('/add',checkLogin,function(req,res){
+	//10.3
+	let article = req.body;
+	//这篇文章的作者是当前的登录用户
+	article.user = req.session.user._id;
+	
+	//10.5 调用Article模型的create方法来进行保存
+	Article.create(article,function(err,doc){
+		if(err){
+			req.flash('error',err);
+			res.redirect('back');
+		}else{
+			req.flash('success','文章发表成功');
+			res.redirect('/');
+		}
+	})
+});
+
 module.exports = router;
