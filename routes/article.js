464f6d75b7132ec180bf0a110981342d06f29b86
zhu-xing-xing <zhuxingxing3721@163.com>
2017-10-16 12:53:27 +0800

    '13,实现更新文章功能'

--- routes/article.js
+++ routes/article.js
@@ -4,7 +4,7 @@ let router = express.Router();
 let {Article} =require('../model');//10.4 导入模型,并解构出来
 
 router.get('/add',checkLogin,function(req,res){//7.10
-	res.render('article/add',{title:'发表文章'});//不能写/或者./  不用写后缀
+	res.render('article/add',{title:'发表文章',article:{}});//不能写/或者./  不用写后缀
 });
 
 //10.1 写路由
@@ -30,7 +30,7 @@ router.post('/add',checkLogin,function(req,res){
 //localhost:8080/article     /detail/59ddce0be97e4734946ba2af
 router.get('/detail/:_id',function(req,res){
 	let _id = req.params._id;//获取路径参数_id
-	console.log(_id);
+	
 	Article.findById(_id,function(err,article){
 		if(err){
 			req.flash('error',err);
@@ -51,6 +51,30 @@ router.get('/delete/:_id',function(req,res){
 		}else{
 			req.flash('success','删除文章成功');
 			res.redirect('/');//成功后跳转到首页
+		}
+	})
+});
+
+//更新按钮的路由  当点击更新按钮时,会请求此路由
+router.get('/update/:_id',function(req,res){
+	let _id = req.params._id;  //先获取要路径里的要更改文章的id
+	Article.findById(_id,function(err,article){//根据id查询对应的文章对象,查询出来的是json对象
+		//增加和更新复用add模版(方便以后统一更改字段...)
+		res.render('article/add',{title:'更新文章',article});//把内容也传进去是为了回显文章的原来内容
+	})
+});
+
+//更新文章之后,进行提交:
+router.post('/update/:_id',function(req,res){
+	let _id = req.params._id;
+	let article = req.body;//更新后的值(只有title和content,没有createAt和user)
+	Article.update({_id},article,function(err,result){  //更新后的内容会覆盖原有重复的的内容
+		if(err){
+			req.flash('error',err);
+			res.redirect('back');
+		}else{
+			req.flash('success','文章更新成功');
+			res.redirect('/article/detail/'+_id);//成功后跳转回更改文章后的文章详情页
 		}
 	})
 });
